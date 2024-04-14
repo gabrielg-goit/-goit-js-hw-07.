6f0131c71fe2d871b71d19617a46db14a6779336
gabrielg-goit <151449282+gabrielg-goit@users.noreply.github.com>
2024-04-14 12:52:41 +0300

    Second

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+function createImagesGallery() {
+  for (const image of galleryItems) {
+    const listElement = document.createElement("li");
+    listElement.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.setAttribute("href", image.original);
+    const img = document.createElement("img");
+
+    img.classList.add("gallery__image");
+    img.setAttribute("src", image.preview);
+    img.setAttribute("alt", image.description);
+
+    list.append(listElement);
+    listElement.append(a);
+    a.append(img);
+
+    a.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const instance = basicLightbox.create(
+        `<img src="${img.src}" alt="${img.alt}">`
+      );
+      instance.show();
+
+      document.addEventListener("keyup", (event) => {
+        if (event.code === "Escape") {
+          instance.close();
+        }
+      });
+    });
+  }
+}
+createImagesGallery();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createImagesGallery() {
+  for (const image of galleryItems) {
+    const listElement = document.createElement("li");
+    listElement.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.setAttribute("href", image.original);
+    a.dataset.source = image.original;
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", image.preview);
+    img.setAttribute("alt", image.description);
+
+    list.append(listElement);
+    listElement.append(a);
+    a.append(img);
+
+    a.addEventListener("click", handler);
+    function handler(event) {
+      event.preventDefault();
+      img.src = image.original;
+    }
+  }
+  const lightBox = new SimpleLightbox(`.gallery a`, {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+createImagesGallery();
